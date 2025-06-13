ad0baee29d32bd86124ca3d438dfcd28e02600d5
Roman Fliunt <romanflyn@gmail.com>
2025-06-13 14:41:16 +0300

    finished result

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,62 +1,80 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import SearchBar from "../SearchBar/SearchBar.tsx";
 import MovieGrid from "../MovieGrid/MovieGrid.tsx";
-import { getMovies } from "../../services/movieService.ts";
-import type { Movie } from "../../types/movie.ts";
+import { fetchMovies } from "../../services/movieService.ts";
+import type { Movie, MoviesResponse } from "../../types/movie.ts";
 import MovieModal from "../MovieModal/MovieModal.tsx";
 import Loader from "../Loader/Loader.tsx";
 import ErrorMessage from "../ErrorMessage/ErrorMessage.tsx";
 import styles from "./App.module.css";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import ReactPaginate from "react-paginate";
+
+type MovieObj = Movie | null;
 
 export default function App() {
-  const [movie, setMovie] = useState<Movie[]>([]);
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [searchValue, setSearchValue] = useState<string>("");
+  const [selectedMovie, setSelectedMovie] = useState<MovieObj>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const notifyError = () =>
-    toast.error("No movies found for your request.", {
-      style: { background: "rgba(125, 183, 255, 0.8)" },
-      icon: "ℹ️",
-    });
+  const { data, isError, isLoading, isSuccess } = useQuery<MoviesResponse>({
+    queryKey: ["myQueryKey", searchValue, currentPage],
+    queryFn: () => fetchMovies(searchValue, currentPage),
+    enabled: !!searchValue && searchValue.trim().length > 0,
+    staleTime: 5 * 60 * 1000,
+    placeholderData: keepPreviousData,
+  });
 
-  const openModal = (movie: Movie) => {
-    setSelectedMovie(movie);
-  };
+  function handleGet(value: string): void {
+    setSearchValue(value);
+    setCurrentPage(1);
+  }
 
-  const closeModal = () => {
-    setSelectedMovie(null);
-  };
-
-  const handleSearch = async (topic: string) => {
-    try {
-      setMovie([]);
-      setError(false);
-      setIsLoading(true);
-      const newMovie = await getMovies(topic);
-      if (newMovie.length === 0) {
-        notifyError();
-      } else {
-        setMovie(newMovie);
-      }
-    } catch {
-      setError(true);
-      setMovie([]);
-    } finally {
-      setIsLoading(false);
+  useEffect(() => {
+    if (isSuccess && data?.results.length === 0) {
+      toast.error("No movies found for your request.");
     }
-  };
+  }, [data, isSuccess]);
+
+  function openModal(movie: Movie): void {
+    setSelectedMovie(movie);
+  }
+
+  function closeModal(): void {
+    setSelectedMovie(null);
+  }
+
+  const shouldShowPagination =
+    typeof data?.total_pages === "number" &&
+    data.total_pages > 1 &&
+    !isError &&
+    !isLoading;
 
   return (
     <div className={styles.app}>
-      <SearchBar onSubmit={handleSearch} />
       <Toaster />
+      <SearchBar onSubmit={handleGet} />
+      {isError && <ErrorMessage />}
       {isLoading && <Loader />}
-      {error && <ErrorMessage />}
-      {movie.length > 0 && <MovieGrid onSelect={openModal} movies={movie} />}
-      {selectedMovie !== null && (
-        <MovieModal onClose={closeModal} movie={selectedMovie} />
+      {isSuccess && data?.results && (
+        <MovieGrid onSelect={openModal} movies={data.results} />
+      )}
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={closeModal} />
+      )}
+      {shouldShowPagination && (
+        <ReactPaginate
+          pageCount={data.total_pages}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={1}
+          onPageChange={({ selected }) => setCurrentPage(selected + 1)}
+          containerClassName={styles.pagination}
+          activeClassName={styles.active}
+          nextLabel="→"
+          previousLabel="←"
+          forcePage={currentPage - 1}
+        />
       )}
     </div>
   );
